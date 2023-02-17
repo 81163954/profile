ce9b03a3814924e6a83aa3e9f015c29e88189778
zhouyu <zyxh01619795@alibaba-inc.com>
2023-02-17 13:40:48 +0800

    ""

--- src/pages/speaking.jsx
+++ src/pages/speaking.jsx
@@ -40,42 +40,42 @@ export default function Speaking() {
         intro="这里会记录一些视频和文章灵感。"
       >
         <div className="space-y-20">
-          <SpeakingSection title="Conferences">
+          <SpeakingSection title="视频">
             <Appearance
-              href="#"
-              title="In space, no one can watch you stream — until now"
-              description="A technical deep-dive into HelioStream, the real-time streaming library I wrote for transmitting live video back to Earth."
-              event="SysConf 2021"
-              cta="Watch video"
+              href="https://www.youtube.com/watch?v=2SuvO4Gi7uY"
+              title="波函数坍塌算法"
+              description="将该算法放入数独中，随着游戏的进行时，进行每个单元格的结果坍缩计算，甚至可以构建一些符合逻辑连接的游戏场景 2D/3D尝试。"
+              event="Youtube 2021"
+              cta="观看视频"
             />
             <Appearance
-              href="#"
-              title="Lessons learned from our first product recall"
-              description="They say that if you’re not embarassed by your first version, you’re doing it wrong. Well when you’re selling DIY space shuttle kits it turns out it’s a bit more complicated."
-              event="Business of Startups 2020"
-              cta="Watch video"
+              href="https://www.bilibili.com/video/BV1h3411G741"
+              title="软件工程背后的思考"
+              description="探讨了软件工程思想的发展以及《人月神话》中的一些例证，带出了如今软件工程中的几个基本模型。这是一个系列视频，从计算机最初的架构提出到如今浩瀚的发展进程。"
+              event="Bilibili 2021"
+              cta="观看视频"
             />
+              <Appearance
+                  href="https://www.bilibili.com/video/BV1JU4y1E73v"
+                  title="正确认知 ReactHook"
+                  description="把各种 Hook 划分成自变量和因变量，以及组件调用各层级路径中的路径变量，结构化的理解了 React 中的 Hook。"
+                  event="Bilibili 2021"
+                  cta="观看视频"
+              />
           </SpeakingSection>
-          <SpeakingSection title="Podcasts">
+          <SpeakingSection title="文章">
             <Appearance
-              href="#"
-              title="Using design as a competitive advantage"
-              description="How we used world-class visual design to attract a great team, win over customers, and get more press for Planetaria."
-              event="Encoding Design, July 2022"
-              cta="Listen to podcast"
+              href="https://github.com/justjavac/free-programming-books-zh_CN"
+              title="免费的程序员编程类书籍"
+              description="包含各种程序员从入门进阶的学习网站，资料"
+              event="Github 2022"
+              cta="入口"
             />
             <Appearance
-              href="#"
-              title="Bootstrapping an aerospace company to $17M ARR"
-              description="The story of how we built one of the most promising space startups in the world without taking any capital from investors."
-              event="The Escape Velocity Show, March 2022"
-              cta="Listen to podcast"
-            />
-            <Appearance
-              href="#"
-              title="Programming your company operating system"
-              description="On the importance of creating systems and processes for running your business so that everyone on the team knows how to make the right decision no matter the situation."
-              event="How They Work Radio, September 2021"
+              href="https://i.linuxtoy.org/docs/guide/index.html"
+              title="开源世界旅行手册"
+              description="技术旅行文档，包括一些基本教程，编程的实用方案以及开源运动史，你可以把这本书当成一个旅行。"
+              event="Github 2009"
               cta="Listen to podcast"
             />
           </SpeakingSection>
